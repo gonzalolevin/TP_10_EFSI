5742487dc5fa872d33336abc7c13ac7966db87a4
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-08 12:07:03 -0300

    proyecto css

--- react-footer/src/Proyectos.js
+++ react-footer/src/Proyectos.js
@@ -16,7 +16,18 @@ const Proyectos = () => {
                                 <a href={proyecto.url}></a>
                                 <img src={proyecto.imagen} alt={proyecto.titulo} />
                                 <a href={proyecto.url} className="btn btn-primary">Ver proyecto</a>
+                                
+                                <div class="love">
+                                    <input id="switch" type="checkbox"/>
+                                        <label class="love-heart" for="switch">
+                                            <i class="left"></i>
+                                            <i class="right"></i>
+                                            <i class="bottom"></i>
+                                            <div class="round"></div>
+                                        </label>
+                                </div>
                             </div>
+                            
                         </div>
                     )
                 })
