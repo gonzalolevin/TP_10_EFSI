0d55d886cd1d5f583f90141441be7ffa41b0107f
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-15 12:10:28 -0300

    avances

--- react-footer/src/Home.js
+++ react-footer/src/Home.js
@@ -1,12 +1,37 @@
-import { Link } from "react-router-dom";
-const Home =()=>{
+import React, { useEffect } from "react";
+import './Home.css'; // Asegúrate de tener el archivo CSS correspondiente
+import './Layout.css'
+import Footer from "./components/Footer";
+import "./components/Footer.css";
 
-    return(
-        <div>
-            <h1>Esta es la pagina de inicio</h1>  
+const App = () => {
+    useEffect(() => {
+        // Cualquier lógica de inicialización que necesites
+    }, []);
+
+    return (
+        <div className="container">
+            <header>
+                <img src="tu-imagen.jpg" alt="Tu Nombre" />
+                <h1>Tu Nombre</h1>
+                <p>Desarrollador Web</p>
+            </header>
+
+            <section className="about">
+                <h2>Acerca de Mí</h2>
+                <p>Breve descripción sobre ti y tus habilidades.</p>
+            </section>
+
+            <section className="languages">
+                <h2>Lenguajes de Programación</h2>
+                <div className="language">HTML</div>
+                <div className="language">CSS</div>
+                <div className="language">JavaScript</div>
+                {/* Agrega aquí más lenguajes según tus habilidades */}
+            </section>
+            <Footer/>
         </div>
     );
 }
-export default Home;
 
-
+export default App;
--- react-footer/src/Proyectos.js
+++ react-footer/src/Proyectos.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Creaciones from './Creaciones.json'; // Importa el archivo JSON
 import './Proyectos.css'; // Importa el archivo CSS
+import './Home.css'; // Asegúrate de tener el archivo CSS correspondiente
+import './Layout.css'
+import Footer from "./components/Footer";
+import "./components/Footer.css";
+
 
 const Proyectos = () => {
     return (
@@ -17,21 +22,21 @@ const Proyectos = () => {
                                 <img src={proyecto.imagen} alt={proyecto.titulo} />
                                 <a href={proyecto.url} className="btn btn-primary">Ver proyecto</a>
                                 
-                                <div class="love">
-                                    <input id="switch" type="checkbox"/>
-                                        <label class="love-heart" for="switch">
-                                            <i class="left"></i>
-                                            <i class="right"></i>
-                                            <i class="bottom"></i>
-                                            <div class="round"></div>
-                                        </label>
+                                <div className={`love ${proyecto.isSmall ? 'small' : ''}`} onClick={(e) => e.stopPropagation()}>
+                                    <input id={`switch${index}`} type="checkbox" />
+                                    <label className="love-heart" htmlFor={`switch${index}`}>
+                                        <i className="left"></i>
+                                        <i className="right"></i>
+                                        <i className="bottom"></i>
+                                        <div className="round"></div>
+                                    </label>
                                 </div>
                             </div>
-                            
                         </div>
                     )
                 })
             }
+            <Footer/>
         </div>
     );
 }
